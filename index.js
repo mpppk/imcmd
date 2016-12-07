92b5ee930f77795e4639c94ca125abdb60c7e738
mpppk <niboshiporipori@gmail.com>
2016-12-07 23:58:42 +0900

    Add imcut command

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-/**
- * Created by yuki on 12/6/16.
- */
+const program = require('commander');
+
+program
+    .version('0.0.1')
+    .option('-i, --input', 'input')
+    .parse(process.argv);
+
+console.log(`mcut f=$(head -1 ${program.input} | tr ',' '\\n' | tr -d ' ' | peco ) < ${program.input}`);
