18661e23af90496dab17acb686d3f009faf2d8b9
mpppk <niboshiporipori@gmail.com>
2016-12-08 01:28:58 +0900

    Allow stdin

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 #!/usr/bin/env node
 
+const exec = require('child_process').exec;
 const execSync = require('child_process').execSync;
+const fs = require('fs');
 const program = require('commander');
 
 program
     .version('0.0.1')
     .option('-i, --input [file path]', 'input')
+    .option('-r, --remove', 'remove')
     .parse(process.argv);
 
-const colName = execSync(`head -1 ${program.input} | tr ',' '\\n' | tr -d ' ' | peco`).toString().replace('\n', '');
-const cmd = `mcut f=${colName} < ${program.input}`;
-console.log(cmd);
-console.log(execSync(cmd).toString());
+const inputData = fs.readFileSync(program.input ? program.input : '/dev/stdin', 'utf8');
+const headers = inputData.split('\n')[0].replace(/,/g, '\n');
+const option = program.remove ? '-r ' : '';
+const colNames = execSync(`peco`, {input: headers})
+    .toString()
+    .replace(/\n/g, ',')
+    .slice(0, -1);
+const cmd = `mcut ${option}f=${colNames}`;
+console.log(execSync(cmd, {input: inputData}).toString());
