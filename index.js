6217951deec104d7ada57c9e8266d25abd763e8a
mpppk <niboshiporipori@gmail.com>
2016-12-08 00:09:20 +0900

    Add -i option

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+#!/usr/bin/env node
+
+const execSync = require('child_process').execSync;
 const program = require('commander');
 
 program
     .version('0.0.1')
-    .option('-i, --input', 'input')
+    .option('-i, --input [file path]', 'input')
     .parse(process.argv);
 
-console.log(`mcut f=$(head -1 ${program.input} | tr ',' '\\n' | tr -d ' ' | peco ) < ${program.input}`);
+const colName = execSync(`head -1 ${program.input} | tr ',' '\\n' | tr -d ' ' | peco`).toString().replace('\n', '');
+const cmd = `mcut f=${colName} < ${program.input}`;
+console.log(cmd);
+console.log(execSync(cmd).toString());
